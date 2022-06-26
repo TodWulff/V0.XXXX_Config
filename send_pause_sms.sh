f90551cdc37836612b2d163e66131581d5c0a3d2
Tod Wulff <tod.wulff@engineer.com>
2022-06-26 14:21:43 -0400

    printer event - automated config backup

--- send_pause_sms.sh
+++ send_pause_sms.sh
@@ -1,1 +1,1 @@
-echo "v0.XXXX ALERT: Paused - Attention Needed" | /home/pi/klipper_config/twilio-sms.sh +12489810005
+echo "v0.XXXX ALERT: Paused - Attention Needed" | /home/fly/klipper_config/twilio-sms.sh +12489810005
