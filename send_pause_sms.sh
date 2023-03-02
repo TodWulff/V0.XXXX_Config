f235f7b8b261e64aa46f007e1f1d0dd02b222e27
TodWulff <tod.wulff@gmail.com>
2023-03-02 11:37:44 -0500

    Harmonization Efforts

--- send_pause_sms.sh
+++ send_pause_sms.sh
@@ -1,1 +1,1 @@
-echo "ALERT: Skippy Paused - Attention Needed" | /home/pi/klipper_config/twilio-sms.sh +12489810005
+echo "ALERT: SkippyII Paused - Attention Needed" | /home/pi/klipper_config/twilio-sms.sh +12489810005
